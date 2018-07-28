3e49c4d253ce0c1a8b13e20f9e78dd52bc5c74cb
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-28 07:35:05 +0100

    deploy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,14 +1,15 @@
-@servers(['bahdcasts-nuxt' => ['root@174.138.15.49']])
+@servers(['apis' => ['root@174.138.15.49']])
 
 @php
-$repo = "git@bahdcasts-nuxt.github.com:bahdcoder/bahdcasts-nuxt.git";
-$releaseDir = '/opt/bahdcasts-nuxt/releases';
+$repo = "git@mentortemple.github.com:mentortemple/mentortemple.git";
+$releaseDir = '/opt/mentortemple/releases';
 $release = 'release_' . date('Y-m-d-H-i');
 @endphp
 
-@macro('deploy', ['on' => 'bahdcasts-nuxt'])
+@macro('deploy', ['on' => 'apis'])
   fetch_repo
   run_npm_install
+  setup_env
   build_nuxt
   restart_pm2
 @endmacro
@@ -24,13 +25,18 @@ $release = 'release_' . date('Y-m-d-H-i');
   npm install;
 @endtask
 
+@task('setup_env')
+  cd {{ $releaseDir }}/{{ $release }};
+  cp .env.prod .env
+@endtask
+
 @task('build_nuxt')
   cd {{ $releaseDir }}/{{ $release }};
   npm run build
 @endtask
 
 @task('restart_pm2')
-  pm2 delete 'bahdcasts-nuxt' || true;
+  pm2 delete 'mentortemple' || true;
   cd {{ $releaseDir }}/{{ $release }};
-  NODE_ENV=production pm2 start npm --name bahdcasts-nuxt -- start
+  NODE_ENV=production pm2 start npm --name mentortemple -- start
 @endtask
